f38576459f9047994fe40eee97b00fd7827c0bf5
Richard Davey <rich@photonstorm.com>
2019-09-06 16:31:29 +0100

    Better audio mime-types for mp3 and opus

--- src/device/audio/canPlayMP3.ts
+++ src/device/audio/canPlayMP3.ts
@@ -1,4 +1,4 @@
 export function canPlayMP3 (audioElement: HTMLAudioElement = document.createElement('audio')): boolean
 {
-    return (audioElement.canPlayType('audio/mpeg') !== '');
+    return (audioElement.canPlayType('audio/mpeg; codecs="mp3"') !== '');
 }
--- src/device/audio/canPlayOpus.ts
+++ src/device/audio/canPlayOpus.ts
@@ -1,4 +1,4 @@
 export function canPlayOpus (audioElement: HTMLAudioElement = document.createElement('audio')): boolean
 {
-    return ((audioElement.canPlayType('audio/ogg; codecs="opus"') !== '') || (audioElement.canPlayType('audio/opus') !== ''));
+    return ((audioElement.canPlayType('audio/ogg; codecs="opus"') !== '') || (audioElement.canPlayType('audio/webm; codecs="opus"') !== ''));
 }
