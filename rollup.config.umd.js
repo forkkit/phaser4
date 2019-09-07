4ef384b6cce668d7b70fbe61005e2c91c8f1f1ac
Richard Davey <rich@photonstorm.com>
2019-09-07 12:20:58 +0100

    Splitting configs up for quicker testing and fine-grained output

---
+++ rollup.config.umd.js
@@ -0,0 +1,27 @@
+import typescript from 'rollup-plugin-typescript2';
+import {terser} from "rollup-plugin-terser";
+
+const pkg = require('./package.json');
+
+export default {
+    input: 'src/index.defaults.ts',
+    output: [
+        { file: pkg.main, name: 'phaser', format: 'umd', sourcemap: true },
+    ],
+    watch: {
+        chokidar: true,
+        include: './src/**'
+    },
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {})
+    ],
+    plugins: [
+        typescript({
+            typescript: require('typescript'),
+            tsconfig: './tsconfig.umd.json',
+            useTsconfigDeclarationDir: true
+        }),
+        //terser()
+    ]
+};
