5048353d37066673f788d04f56e094756e03daf5
Richard Davey <rich@photonstorm.com>
2019-09-10 14:12:18 +0100

    Bundle dist

---
+++ bundles/phaser/dist/index.d.ts
@@ -0,0 +1,18 @@
+import * as Device from '@phaserjs/device';
+import * as DOM from '@phaserjs/dom';
+import { Game } from '@phaserjs/core';
+import { BaseLoader, BaseLoaderState, File, FileState, XHRLoader, XHRSettings } from '@phaserjs/loader';
+import { LoaderPlugin } from '@phaserjs/plugin-loader';
+import * as FileTypes from '@phaserjs/loader-filetypes';
+declare const Loader: {
+    BaseLoader: typeof BaseLoader;
+    BaseLoaderState: typeof BaseLoaderState;
+    File: typeof File;
+    FileState: typeof FileState;
+    XHRLoader: typeof XHRLoader;
+    XHRSettings: typeof XHRSettings;
+    FileTypes: typeof FileTypes;
+    LoaderPlugin: typeof LoaderPlugin;
+};
+export { Device, DOM, Game, Loader };
+//# sourceMappingURL=index.d.ts.map
---
+++ bundles/phaser/dist/index.js
@@ -0,0 +1,19 @@
+//  @namespace Phaser
+import * as Device from '@phaserjs/device';
+import * as DOM from '@phaserjs/dom';
+import { Game } from '@phaserjs/core';
+import { BaseLoader, BaseLoaderState, File, FileState, XHRLoader, XHRSettings } from '@phaserjs/loader';
+import { LoaderPlugin } from '@phaserjs/plugin-loader';
+import * as FileTypes from '@phaserjs/loader-filetypes';
+const Loader = {
+    BaseLoader,
+    BaseLoaderState,
+    File,
+    FileState,
+    XHRLoader,
+    XHRSettings,
+    FileTypes,
+    LoaderPlugin
+};
+export { Device, DOM, Game, Loader };
+//# sourceMappingURL=index.js.map
