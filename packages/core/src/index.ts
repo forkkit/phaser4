43c0cc0c75d44a7a8ff06346c21645759f8d6ccf
Richard Davey <rich@photonstorm.com>
2019-09-10 13:55:39 +0100

    Tidy-up Core export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,7 @@
-//  @namespace Phaser
+//  @namespace Phaser.Core
 
-import * as Device from './device/';
-import * as DOM from './dom/';
 import { Game } from './Game';
-import * as Loader from './loader/';
 
 export {
-    Device,
-    DOM,
     Game,
-    Loader
 };
