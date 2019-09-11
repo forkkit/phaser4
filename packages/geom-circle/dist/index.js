2b3abbb072679baf4369da39fd875ccbf061e633
Richard Davey <rich@photonstorm.com>
2019-09-11 13:26:24 +0100

    Export * for this type of package

--- packages/geom-circle/dist/index.js
+++ packages/geom-circle/dist/index.js
@@ -1,11 +1,29 @@
-import * as Area from './Area';
-import * as Circle from './Circle';
-import * as Circumference from './Circumference';
-import * as Clone from './Clone';
-import * as Contains from './Contains';
-import * as CopyFrom from './CopyFrom';
-import * as Equals from './Equals';
-import * as ICircle from './ICircle';
-import * as Translate from './Translate';
-export { Area, Circle, Circumference, Clone, Contains, CopyFrom, Equals, ICircle, Translate };
+// import * as Area from './Area';
+// import { Circle } from './Circle';
+// import * as Circumference from './Circumference';
+// import * as Clone from './Clone';
+// import * as Contains from './Contains';
+// import * as CopyFrom from './CopyFrom';
+// import * as Equals from './Equals';
+// import * as ICircle from './ICircle';
+// import * as Translate from './Translate';
+// export const GeomCircle {
+//     Area,
+//     Circle,
+//     Circumference,
+//     Clone,
+//     Contains,
+//     CopyFrom,
+//     Equals,
+//     ICircle,
+//     Translate
+// };
+export * from './Area';
+export * from './Circle';
+export * from './Circumference';
+export * from './Clone';
+export * from './Contains';
+export * from './CopyFrom';
+export * from './Equals';
+export * from './Translate';
 //# sourceMappingURL=index.js.map
